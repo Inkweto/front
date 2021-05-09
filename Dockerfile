51631a8055cdd322d5cb99a9c7ab26a73b45e4a8
Juk3n <adekenn3@gmail.com>
2021-05-09 10:43:16 +0200

    files are modified by node user, moved project to upper catalogue, default way of opening is by vscode in container

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,21 @@
-FROM node:12.18.1
-ENV NODE_ENV=development
-
-WORKDIR /workdir/
-
-COPY app/package.json /workdir/
-
+## Build vue project
+FROM node:12.18.1-alpine AS build
+WORKDIR /app/
+COPY ./package.json /app/
 RUN npm install
+COPY . /app/
+RUN npm run build
 
-COPY . .
-CMD [ "npm", "run", "serve" ]
+# Dev user and workspace parameters
+ARG USERNAME=dev
+ARG USER_UID=1000
+ARG USER_GID=$USER_UID
+ARG WORKSPACE=/app
+
+USER node
+
+## Create nginx server - uncomment to run on production
+# FROM nginx:1.19.0-alpine AS prod-stage
+# COPY --from=build /app/dist /usr/share/nginx/html
+EXPOSE 8000
+# CMD ["nginx", "-g", "daemon off;"]
--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,29 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="dark"
       dark
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          :src="require('./assets/logo.png')"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        LogSearch
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/Inkweto/front"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Github</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>

ad3e4266030879a0abc7849ad23cc29b5544a04e
Juk3n <adekenn3@gmail.com>
2021-06-15 22:54:12 +0200

    added simple search form

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
-
+Vue.prototype.$api_url = "http://localhost:5000"
 new Vue({
   vuetify,
   render: h => h(App)
